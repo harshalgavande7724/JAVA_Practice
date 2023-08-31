a129c4ef745c0fb5a48b34a3a1c5a2758eef51ad
Harshal gavande <142582823+harshalgavande7724@users.noreply.github.com>
2023-08-31 14:57:00 +0530

    Add files via upload

---
+++ Asciivalue.java
@@ -0,0 +1,12 @@
+//Write a Java program to print the ASCII value of a given character.//
+class AsciiVal{
+public static void main(String args[])
+{
+char ch1 = 'a';  
+char ch2 = 'b';    
+int asciivalue1 = ch1;  
+int asciivalue2 = ch2;  
+System.out.println("The ASCII value of " + ch1 + " is: " + asciivalue1);  
+System.out.println("The ASCII value of " + ch2 + " is: " + asciivalue2);  
+}
+}
---
+++ Swappingnumber.java
@@ -0,0 +1,14 @@
+//Write a Java program to swap two numbers by using the third variable//
+ class SwappingNumbers{
+public static void main(String args[])
+{
+int a=10,b=20,temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println(" value of a:"+a);
+System.out.println("value of b"+b);
+
+}
+}
+
